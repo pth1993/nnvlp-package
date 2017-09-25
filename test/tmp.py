a08ccf97c921f418aed571f845417005548986fb
pth1993 <phamthaihoang.hn@gmail.com>
2017-09-25 12:28:37 +0700

    first commit

--- test/tmp.py
+++ test/tmp.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import nnvlp
+
+
+nnvlp.download()
 model = nnvlp.NNVLP()
+output = model.predict(u'Ông Nam là giảng viên trường Bách Khoa.', display_format='JSON')
+print output
