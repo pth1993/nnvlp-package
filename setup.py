b196b13de2b8c0a5bbff29bd30910bac2623f58f
pth1993 <phamthaihoang.hn@gmail.com>
2017-09-25 23:13:56 +0700

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
+from setuptools.command.install import install
+import subprocess, shlex
+
+
+class NLTKDownloader(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        subprocess.call(shlex.split("python -m nltk.downloader all"))
+        install.run(self)
+
 
 setup(name='nnvlp',
-      version='0.1',
+      version='0.1.1',
       description='Neural Network-Based Vietnamese Language Processing',
-      url='http://github.com/pth1993',
+      url='http://github.com/pth1993/nnvlp-package',
       author='Hoang Pham',
       author_email='phamthaihoang.hn@gmail.com',
       license='MIT',
@@ -14,4 +24,7 @@ setup(name='nnvlp',
       test_suite='nose.collector',
       tests_require=['nose'],
       include_package_data=True,
+      cmdclass={
+        'install': NLTKDownloader,
+      },
       zip_safe=False)
