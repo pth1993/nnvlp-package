9c3d170c9e2c13fa8c9d225496143a09e2197f29
pth1993 <phamthaihoang.hn@gmail.com>
2017-09-25 16:10:20 +0700

    add package files

--- nnvlp/embedding.sh
+++ nnvlp/embedding.sh
@@ -3,6 +3,16 @@ dir="embedding"
 if [ ! -d "$dir" ]; then
     mkdir "embedding"
 fi
+file="embedding/vectors.npy"
+if [ -f "$file" ]; then
+	echo "$file found."
+else
+	url="https://drive.google.com/uc?export=download&id=0BytHkPDTyLo9WU93NEI1bGhmYmc"
+    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}" -O tmp
+    c=`grep -o "confirm=...." tmp`
+    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${file}"
+    rm cookie.txt tmp
+fi
 file="embedding/words.pl"
 if [ -f "$file" ]; then
     echo "$file found."
